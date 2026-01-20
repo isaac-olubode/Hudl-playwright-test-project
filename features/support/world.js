3794cb228cd8fb0381ed011dc22ba5940804b60d
isaac-olubode <isaac.olubode@gmail.com>
2026-01-20 22:43:44 +0000

    set headless to false

--- features/support/world.js
+++ features/support/world.js
@@ -3,7 +3,7 @@ import { chromium } from 'playwright';
 
 class CustomWorld {
   async init() {
-    this.browser = await chromium.launch({ headless: false });
+    this.browser = await chromium.launch({ headless: true });
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
   }
